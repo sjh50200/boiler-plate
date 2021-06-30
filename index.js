b6937da552e6ea50c8f1cc2d23e6c3fa635cde42
JongHyeon <sjh2273@naver.com>
2021-06-30 21:11:18 +0900

    처음 저장소에 올림

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express') //express module 가져옴
 const app = express() 
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jong:whdtjd95@boilerplate.gfigu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
